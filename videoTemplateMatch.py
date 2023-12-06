13a9d9d4adcc9c9b6a4b877e205ebab9b096716a
JESG-27 <jose.salcido4178@alumnos.udg.mx>
2023-12-06 13:44:45 -0600

    Speed calculation

--- videoTemplateMatch.py
+++ videoTemplateMatch.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 # Load the images in grey scale 
 vid = cv2.VideoCapture('source/sample_video.mp4')
+frame_rate = int(vid.get(cv2.CAP_PROP_FPS))
 template = cv2.imread('source/ball_template.jpg', 0)
 output = cv2.VideoWriter("output.avi", cv2.VideoWriter_fourcc(*'MJPG'), 30, (int(vid.get(3)), int(vid.get(4)))) 
 
@@ -46,6 +47,20 @@ vid.release()
 cv2.destroyAllWindows()
 
 fig = plt.figure()
+positions = [[],[]]
 for position in trajectory:
-    plt.scatter(position[0], -position[1], color='red')
+    positions[0].append(position[0])
+    positions[1].append(-position[1])
+
+plt.scatter(positions[0], positions[1], color='red', s=10)
+
+max = np.argmax(positions[1])
+min = np.argmin(positions[1])
+
+frame_dif = np.absolute(np.absolute(max)-np.absolute(min))
+time = (frame_dif*1)/frame_rate
+distance = float(input("Estimated drop height: "))
+speed = distance/time # speed = distance/time, this is an estimate distance
+print(f"Average speed: {speed:.2f} meters per second")
+
 plt.show()
